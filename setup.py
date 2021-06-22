14799f8b2d559b045627ce79f354fc94cb4ec3c9
Khaled Alamin <khalid.s.alamin@gmail.com>
2021-06-23 01:00:28 +0200

    refactor: package uploading

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup
 
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["matplotlib", "numpy", "math"]
+
+
 setup(name='gaus_binom_dist',
       version='0.1',
       description='Gaussian & Binomial distributions',
+      long_description = readme,
+      install_requires = requirements,
       packages=['gaus_binom_dist'],
       zip_safe=False)
